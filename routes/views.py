29f43814f0bad49eb399df06fdee26097ee31600
ZhiQiYang <59477728+ZhiQiYang@users.noreply.github.com>
2025-03-29 18:51:51 +0800

    Update views.py

--- routes/views.py
+++ routes/views.py
@@ -1,17 +1,13 @@
+# routes/views.py
 from flask import Blueprint, render_template, request
-import os
+from config import Config
 
 views_bp = Blueprint('views', __name__)
 
-# 環境變數
-LIFF_ID = os.environ.get('LIFF_ID')
-GROUP_LIFF_ID = os.environ.get('GROUP_LIFF_ID')
-GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
-
 @views_bp.route('/checkin')
 def checkin_page():
-    return render_template('checkin.html', liff_id=LIFF_ID, google_maps_api_key=GOOGLE_MAPS_API_KEY)
+    return render_template('checkin.html', liff_id=Config.LIFF_ID, google_maps_api_key=Config.GOOGLE_MAPS_API_KEY)
 
 @views_bp.route('/group')
 def group_page():
-    return render_template('group.html', liff_id=GROUP_LIFF_ID)
+    return render_template('group.html', liff_id=Config.GROUP_LIFF_ID)
