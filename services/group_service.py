06ac8db2dfbe3fea8c7c45efdd296d29545145bb
ZhiQiYang <59477728+ZhiQiYang@users.noreply.github.com>
2025-03-29 18:08:35 +0800

    Update group_service.py

--- services/group_service.py
+++ services/group_service.py
@@ -1,18 +1,18 @@
 import json
 from utils.file_helper import ensure_file_exists
 from config import GROUP_MESSAGES_FILE
+from db.storage import ensure_file_exists, load_json, save_json
+from config import Config
 
-# 確保群組訊息檔案存在
-def ensure_group_messages_file():
-    ensure_file_exists(GROUP_MESSAGES_FILE, {"messages": []})
+def get_recent_messages(count=20):
+    """取得最近群組訊息"""
+    data = load_json(Config.GROUP_MESSAGES_FILE, {"messages": []})
+    return data["messages"][-count:]
 
-# 儲存群組訊息
 def save_group_message(user_id, user_name, message, timestamp):
-    ensure_group_messages_file()
-
-    with open(GROUP_MESSAGES_FILE, 'r') as f:
-        data = json.load(f)
-
+    """儲存群組訊息"""
+    data = load_json(Config.GROUP_MESSAGES_FILE, {"messages": []})
+    
     new_message = {
         "user_id": user_id,
         "user_name": user_name,
@@ -20,10 +20,11 @@ def save_group_message(user_id, user_name, message, timestamp):
         "timestamp": timestamp
     }
     data["messages"].append(new_message)
-
+    
     # 只保留最近 100 筆
     data["messages"] = data["messages"][-100:]
-
+    
+    save_json(Config.GROUP_MESSAGES_FILE, data)
     with open(GROUP_MESSAGES_FILE, 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
