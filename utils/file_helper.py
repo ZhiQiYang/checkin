2e0334c2cab41f72e4f139ddc3b23c1e94683b27
ZhiQiYang <59477728+ZhiQiYang@users.noreply.github.com>
2025-03-29 17:41:02 +0800

    Update file_helper.py

--- utils/file_helper.py
+++ utils/file_helper.py
@@ -13,3 +13,20 @@ def load_json(filename):
 def save_json(filename, data):
     with open(filename, 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
+
+def load_json_file(filename, default=None):
+    """
+    加載 JSON 文件，如果文件不存在或格式錯誤，返回默認值
+    """
+    try:
+        return load_json(filename)
+    except FileNotFoundError:
+        if default is not None:
+            return default
+        raise
+    except json.JSONDecodeError:
+        if default is not None:
+            return default
+        raise
+
+# 為向後兼容添加別名
