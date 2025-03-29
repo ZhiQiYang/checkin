b9edc884cfda8e44c44938bc3bf66934346e3322
ZhiQiYang <59477728+ZhiQiYang@users.noreply.github.com>
2025-03-29 17:09:28 +0800

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,4 +1,4 @@
 # 可根據需要將常用服務快速引入
-from .checkin_service import save_checkin_record
+from .checkin_service import process_checkin
 from .notification_service import send_line_notification
 from .group_service import save_group_message, get_recent_messages
