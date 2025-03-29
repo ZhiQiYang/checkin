70e47ad089dcdfe8c3f9be4300c042e36e4cf054
ZhiQiYang <59477728+ZhiQiYang@users.noreply.github.com>
2025-03-29 15:04:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()  # 載入 .env 檔案中的環境變數
-
-# LINE API 設定
-LINE_LOGIN_CHANNEL_ID = os.environ.get('LINE_LOGIN_CHANNEL_ID')
-LINE_LOGIN_CHANNEL_SECRET = os.environ.get('LINE_LOGIN_CHANNEL_SECRET')
-LIFF_ID = os.environ.get('LIFF_ID')
-MESSAGING_CHANNEL_ACCESS_TOKEN = os.environ.get('MESSAGING_CHANNEL_ACCESS_TOKEN')
-LINE_GROUP_ID = os.environ.get('LINE_GROUP_ID')
+class Config:
+    APP_URL = os.environ.get('APP_URL', 'https://你的應用.onrender.com')
+    PORT = int(os.environ.get('PORT', 5000))
+    PING_INTERVAL = 840  # 14 minutes
+    LINE_LOGIN_CHANNEL_ID = os.environ.get('LINE_LOGIN_CHANNEL_ID')
+    LINE_LOGIN_CHANNEL_SECRET = os.environ.get('LINE_LOGIN_CHANNEL_SECRET')
+    LINE_GROUP_ID = os.environ.get('LINE_GROUP_ID')
+    MESSAGING_CHANNEL_ACCESS_TOKEN = os.environ.get('MESSAGING_CHANNEL_ACCESS_TOKEN')
+    GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY', '')
+    LIFF_ID = os.environ.get('LIFF_ID')
+    GROUP_LIFF_ID = os.environ.get('GROUP_LIFF_ID')
