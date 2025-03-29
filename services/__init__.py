ea962fa8edc9588193c89cf1b1b09ab0761304a8
ZhiQiYang <59477728+ZhiQiYang@users.noreply.github.com>
2025-03-29 15:40:14 +0800

    Create __init__.py

---
+++ services/__init__.py
@@ -0,0 +1,4 @@
+# 可根據需要將常用服務快速引入
+from .checkin_service import save_checkin_record
+from .notification_service import send_line_notification
+from .group_service import save_group_message, get_recent_messages
