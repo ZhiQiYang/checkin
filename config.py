0902984147e1d329893611cdcb294a9cf4b8c9e7
ZhiQiYang <59477728+ZhiQiYang@users.noreply.github.com>
2025-03-29 15:13:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 import os
 
-class Config:
-    APP_URL = os.environ.get('APP_URL', 'https://你的應用.onrender.com')
-    PORT = int(os.environ.get('PORT', 5000))
-    PING_INTERVAL = 840  # 14 minutes
-    LINE_LOGIN_CHANNEL_ID = os.environ.get('LINE_LOGIN_CHANNEL_ID')
-    LINE_LOGIN_CHANNEL_SECRET = os.environ.get('LINE_LOGIN_CHANNEL_SECRET')
-    LINE_GROUP_ID = os.environ.get('LINE_GROUP_ID')
-    MESSAGING_CHANNEL_ACCESS_TOKEN = os.environ.get('MESSAGING_CHANNEL_ACCESS_TOKEN')
-    GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY', '')
-    LIFF_ID = os.environ.get('LIFF_ID')
-    GROUP_LIFF_ID = os.environ.get('GROUP_LIFF_ID')
+LINE_LOGIN_CHANNEL_ID = os.getenv("LINE_LOGIN_CHANNEL_ID")
+LINE_LOGIN_CHANNEL_SECRET = os.getenv("LINE_LOGIN_CHANNEL_SECRET")
+LINE_GROUP_ID = os.getenv("LINE_GROUP_ID")
+LIFF_ID = os.getenv("LIFF_ID")
+GROUP_LIFF_ID = os.getenv("GROUP_LIFF_ID")
+MESSAGING_CHANNEL_ACCESS_TOKEN = os.getenv("MESSAGING_CHANNEL_ACCESS_TOKEN")
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY", "")
+APP_URL = os.getenv("APP_URL", "https://你的應用名稱.onrender.com")
+PORT = int(os.getenv("PORT", 5000))
